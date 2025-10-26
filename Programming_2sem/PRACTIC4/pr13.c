8e1a183134bbf3e602c0ae879fed29efcdb52d46
Fertry <iglfox@yandex.ru>
2025-10-26 22:24:20 +0300

    add practics

---
+++ Programming_2sem/PRACTIC4/pr13.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void func(int *sum, int *count) {
+    int num;
+    printf("Input el => ");
+    scanf("%d", &num); 
+    
+    if (num == 0) {    
+        return;
+    }
+    
+    *sum += num;
+    *count += 1;
+    
+    func(sum, count);
+}
+
+int main() {
+    int sum = 0;
+    int count = 0;
+    
+    func(&sum, &count);
+    
+    if (count == 0) {
+        printf("Empty.\n");
+    } else {
+        double average = (double)sum / count;
+        printf("Middle: %.2f\n", average);
+    }
+    
+    return 0;
+}
